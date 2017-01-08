317900d4a690526a64361ade66d239ac2ec345e1
Brian Barrow <briancbarrow@gmail.com>
2017-01-07 18:55:03 -0700

    added Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PuppiesComponent } from './components/puppies/puppies.component';
+import { HomeComponent } from './components/home/home.component';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'puppies',
+    component: PuppiesComponent
+  }
+], { useHash: true })
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PuppiesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    rootRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/puppy/puppy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'puppies',
+  templateUrl: './puppies.component.html',
+  styleUrls: ['./puppies.component.css']
+})
+
+export class PuppiesComponent {
+  constructor() {}
+
+
+}
